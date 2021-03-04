4f0d9329223527e01b97080525ee9bf3e7116fff
The Dad <Shihen1611@gmail.com>
2021-03-03 20:06:32 -0600

    first push

---
+++ SelectionFun/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SelectionFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var favoriteNum = 7;
+            Console.WriteLine("Can you guess my favorite number?");
+            Console.WriteLine($"Enter it here");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < 7)
+            { Console.WriteLine("Good guess, but it's too low"); }
+
+
+
+            else if (userInput > 7)
+            { Console.WriteLine("Good guess, but it's too high"); }
+
+
+
+
+
+            else
+            Console.WriteLine("Bingo!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
